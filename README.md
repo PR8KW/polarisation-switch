afd74896d12faaf0627cb1020549fc3ca5d44308
PR8KW <48393217+PR8KW@users.noreply.github.com>
2024-03-29 20:18:19 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
 # Polarisation Switch Control System
-Code used to control the polarisation switch for my Wimo X-Quads using a [4 Channel Relay Raspberry Pi Hat](https://amzn.to/311bw4H)
+Based on Peter 2M0SQL Project https://github.com/magicbug/polarisation-switch 
+MANY THANKS Peter :)
 
-Made using Python Flask for the web server functions.
+Control for the Wimo polarisation switch and LNA power using a [Raspberry Pi 8ch Relay Rxpansion Board](https://a.aliexpress.com/_mLenEXs)
+
+Using Python Flask for the web server functions.
 
 ## Polarisation Options
 * Horizontal 
 * Vertical
 * RHCP
 * LHCP
+* LNA ON/OFF
+
+
+## How to Install
+I used this commands on my RPI 3B+ with Raspberry Pi OS Lite(Legacy - Bullseye):
+
+sudo apt update
+sudo apt install git python3-pip
+pip install flask flask-cors waitress
+git clone https://github.com/PR8KW/polarisation-switch.git
+cd polarisation-switch
+sudo cp relays.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable relays.service
+sudo systemctl start relays.service
+
+## How to Use
+Just access from any web browser on your local network: http://"your rpi ip":5000
+
+I use port 5000, if you need to change, just edit app.py file.
+
+FOR PRIVATE/INTERNAL NETWORK ONLY.
+if your need external access, please read flask documentation, for security reasons...
+
+Maybe in future will include some interconnection drawings...
+
+73 de PR8KW
--- app.py
+++ app.py
@@ -8,8 +8,10 @@ Complete project details: https://github.com/magicbug/polarisation-switch
 '''
 
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, request
+from os import popen
+from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
+from waitress import serve
 
 app = Flask(__name__)
 CORS(app)
@@ -18,155 +20,167 @@ GPIO.setmode(GPIO.BCM)
 
 # Create a dictionary called pins to store the pin number, name, and pin state:
 pins = {
-   4 : {'name' : 'GPIO 4', 'state' : GPIO.LOW},
-   22 : {'name' : 'GPIO 22', 'state' : GPIO.LOW},
-   6 : {'name' : 'GPIO 4', 'state' : GPIO.LOW},
-   26 : {'name' : 'GPIO 22', 'state' : GPIO.LOW},
+   5 : {'name' : 'GPIO 5', 'state' : GPIO.LOW},
+   6 : {'name' : 'GPIO 6', 'state' : GPIO.LOW},
+   13 : {'name' : 'GPIO 13', 'state' : GPIO.LOW},
+   16 : {'name' : 'GPIO 16', 'state' : GPIO.LOW},
+   19 : {'name' : 'GPIO 19', 'state' : GPIO.LOW},
+   20 : {'name' : 'GPIO 20', 'state' : GPIO.LOW},
+   21 : {'name' : 'GPIO 21', 'state' : GPIO.LOW},
+   26 : {'name' : 'GPIO 26', 'state' : GPIO.LOW},
    }
 
-vartwo = ''
-message = ''
+Phase_70cm = 'v'
+Phase_2m = 'v'
+LNA_2m = 'off'
+LNA_70cm = 'off'
+temp = '0'
 
-# Set each pin as an output and make it low:
+def get_temp():
+    temperature = popen('vcgencmd measure_temp').readline()
+    return (temperature.replace("\n", "").replace("temp=", "").replace("'C", ""))
+
+def set_2m_v():
+	GPIO.output(5, GPIO.HIGH)
+	GPIO.output(6, GPIO.HIGH)
+	GPIO.output(13, GPIO.HIGH)
+
+def set_2m_h():
+	GPIO.output(5, GPIO.LOW)
+	GPIO.output(6, GPIO.HIGH)
+	GPIO.output(13, GPIO.HIGH)
+
+def set_2m_rhcp():
+	GPIO.output(5, GPIO.LOW)
+	GPIO.output(6, GPIO.LOW)
+	GPIO.output(13, GPIO.HIGH)
+
+def set_2m_lhcp():
+	GPIO.output(5, GPIO.LOW)
+	GPIO.output(6, GPIO.LOW)
+	GPIO.output(13, GPIO.LOW)
+
+def set_2m_lnaON():
+	GPIO.output(16, GPIO.LOW)
+
+def set_2m_lnaOFF():
+	GPIO.output(16, GPIO.HIGH)
+
+def set_70cm_v():
+	GPIO.output(19, GPIO.HIGH)
+	GPIO.output(20, GPIO.HIGH)
+	GPIO.output(21, GPIO.HIGH)
+
+def set_70cm_h():
+	GPIO.output(19, GPIO.LOW)
+	GPIO.output(20, GPIO.HIGH)
+	GPIO.output(21, GPIO.HIGH)
+
+def set_70cm_rhcp():
+	GPIO.output(19, GPIO.LOW)
+	GPIO.output(20, GPIO.LOW)
+	GPIO.output(21, GPIO.HIGH)
+
+def set_70cm_lhcp():
+	GPIO.output(19, GPIO.LOW)
+	GPIO.output(20, GPIO.LOW)
+	GPIO.output(21, GPIO.LOW)
+
+def set_70cm_lnaON():
+	GPIO.output(26, GPIO.LOW)
+
+def set_70cm_lnaOFF():
+	GPIO.output(26, GPIO.HIGH)
+
+# Set each pin as an output and make it high:
 for pin in pins:
    GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+   GPIO.output(pin, GPIO.HIGH)
 
 @app.route("/")
 def main():
-   global vartwo 
-   global message
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
+   global Phase_70cm 
+   global Phase_2m
+   global LNA_2m
+   global LNA_70cm
+   global temp
+   temp = get_temp()
+   
    templateData = {
-      'pins' : pins
+      'Phase_2m' : Phase_2m,
+      'Phase_70cm': Phase_70cm,
+      'LNA_2m': LNA_2m,
+      'LNA_70cm': LNA_70cm,
+      'temp': temp
       }
    # Pass the template data into the template main.html and return it to the user
    return render_template('main.html', **templateData)
 
-@app.route("/phase/2m/<changephase>")
-def changephase(changephase):
-   global vartwo 
-   global message
-
-   deviceName = changephase
-
-   if changephase== "rhcp":
-      # Set the pin high:
-      GPIO.output(4, GPIO.HIGH)
-      GPIO.output(22, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "rhcp"
-
-   if changephase == "lhcp":
-      # Set the pin high:
-      GPIO.output(4, GPIO.HIGH)
-      GPIO.output(22, GPIO.LOW)
-      # Save the status message to be passed into the template:
-      message = "lhcp"
-
-   if changephase == "v":
-      GPIO.output(4, GPIO.LOW)
-      GPIO.output(22, GPIO.LOW)
-      message = "v"
-
-   if changephase == "h":
-      GPIO.output(4, GPIO.LOW)
-      GPIO.output(22, GPIO.HIGH)
-      message = "h"
-
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'pins' : pins,
-      'message' : message,
-      'vartwo': vartwo
-   }
-
-   return render_template('main.html', **templateData)
-
-@app.route("/70cm/<phase>")
-def phase(phase):
-   global vartwo 
-   global message
+@app.route("/2m/<phase>")
+def set2mphase(phase):
+   global Phase_2m
+   global LNA_2m
 
    deviceName = phase
 
    if phase== "rhcp":
-      # Set the pin high:
-      GPIO.output(6, GPIO.HIGH)
-      GPIO.output(26, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      vartwo = "rhcp"
+      set_2m_rhcp()
+      Phase_2m = "rhcp"
 
    if phase == "lhcp":
-      # Set the pin high:
-      GPIO.output(6, GPIO.HIGH)
-      GPIO.output(26, GPIO.LOW)
-      # Save the status message to be passed into the template:
-      vartwo = "lhcp"
+      set_2m_lhcp()
+      Phase_2m = "lhcp"
 
    if phase == "v":
-      GPIO.output(6, GPIO.LOW)
-      GPIO.output(26, GPIO.LOW)
-      vartwo = "v"
+      set_2m_v()
+      Phase_2m = "v"
 
    if phase == "h":
-      GPIO.output(6, GPIO.LOW)
-      GPIO.output(26, GPIO.HIGH)
-      vartwo = "h"
+      set_2m_h()
+      Phase_2m = "h"
 
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
+   if phase == "lnaon":
+      set_2m_lnaON()
+      LNA_2m = "on"
 
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'pins' : pins,
-      'message' : message,
-      'vartwo': vartwo
-   }
+   if phase == "lnaoff":
+      set_2m_lnaOFF()
+      LNA_2m = "off"
 
-   return render_template('main.html', **templateData)
+   return redirect("/")
 
-@app.route("/<changePin>/<action>")
-def action(changePin, action):
-   global vartwo 
-   global message
+@app.route("/70cm/<phase>")
+def set70cmphase(phase):
+   global Phase_70cm 
+   global LNA_70cm
 
-   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
-   # Get the device name for the pin being changed:
-   deviceName = pins[changePin]['name']
-   # If the action part of the URL is "on," execute the code indented below:
-   if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
-   if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+   deviceName = phase
 
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
+   if phase== "rhcp":
+      set_70cm_rhcp()
+      Phase_70cm = "rhcp"
 
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'pins' : pins
-   }
+   if phase == "lhcp":
+      set_70cm_lhcp()
+      Phase_70cm = "lhcp"
 
-   return render_template('main.html', **templateData)
+   if phase == "v":
+      set_70cm_v()
+      Phase_70cm = "v"
 
-@app.route("/api/status")
-def api():
- return "Running"
+   if phase == "h":
+      set_70cm_h()
+      Phase_70cm = "h"
+
+   if phase == "lnaon":
+      set_70cm_lnaON()
+      LNA_70cm = "on"
+
+   if phase == "lnaoff":
+      set_70cm_lnaOFF()
+      LNA_70cm = "off"
+
+   return redirect("/")
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', debug=True)
+   serve(app, host='0.0.0.0', port=5000)
